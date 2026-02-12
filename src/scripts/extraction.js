10a0a01625767defc8feefe53b28c38eccff0fa2
christopher-artis <carti002@odu.edu>
2026-02-12 12:10:09 -0500

    extract first P tag from website

--- src/scripts/extraction.js
+++ src/scripts/extraction.js
@@ -1,7 +1,20 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
 
+async function extract(url) {
+    try {
+        const response = await axios.get(url)
+        const html = response.data 
+        const $ = cheerio.load(html)
 
-function extract(url) {
-    return null
+        const $p = $('p:first').text()
+        const $p_clean = $p.replace(/\s+/g, ' ').trim()
+
+        return $p_clean
+
+    } catch (error) {
+        console.error(`Error fetching page: ${error}.`)
+    }
 }
 
 module.exports = {extract}
--- tests/scripts/test_extraction.test.js
+++ tests/scripts/test_extraction.test.js
@@ -2,7 +2,8 @@ const test = require('node:test');
 const assert = require("assert")
 const {extract} = require("../../src/scripts/extraction")
 
-test("extracts text from URL", () => {
-    let content = extract("www.google.com")
-    assert.strictEqual(content, null)
+test("extracts text from URL", async () => {
+    let content = await extract("https://mrcarr87.github.io/CS-410---Team-Blue---Misinformation-Project/")
+    let text = "Misinformation is an increasing problem in today's society. With social media, internet access, and mass media now the forefront of public knowledge, the threat of misleading or outright wrong information is the new digital threat. This project serves as an active defense against this world-wide threat and a call to do research on all topics the public is faced with."
+    assert.strictEqual(content, text)
 })
