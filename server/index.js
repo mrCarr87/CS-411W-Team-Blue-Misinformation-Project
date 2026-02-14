a43100f751256d3239ef618313959a280a69e452
christopher-artis <carti002@odu.edu>
2026-02-13 21:22:41 -0500

    queries added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
-const express = require("express")
+import express from "express"
+import {article_extract} from "./scripts/extraction.js"
 const app = express()
 const port = 3000
 
+
+
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!")
+})
+
+app.get("/extract", async (req, res) => {
+    const link = req.query.link
+    const article = await article_extract(link)
+    res.send(article)
+    
+    
 })
 
 app.listen(port, () => {
-    console.log(`App listening at http://locahost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
--- server/scripts/extraction.js
+++ server/scripts/extraction.js
@@ -1,36 +1,17 @@
-const axios = require("axios")
-const cheerio = require("cheerio")
+import {extract} from "@extractus/article-extractor"
+import striptags from "striptags"
 
-async function extract_first(url) {
+
+async function article_extract(url) {
     try {
-        const response = await axios.get(url)
-        const html = response.data 
-        const $ = cheerio.load(html)
-
-        const $p = $('p:first').text()
-        const $p_clean = $p.replace(/\s+/g, ' ').trim()
-
-        return $p_clean
-
+        const data = await extract(url)
+        
+        const content = data.content
+        const stripped = striptags(content)
+        return stripped
     } catch (error) {
-        console.error(`Error fetching page: ${error}.`)
+        console.log(error)
     }
+
 }
-
-async function extract_all(url) {
-    try {
-        const response = await axios.get(url)
-        const html = response.data 
-        const $ = cheerio.load(html)
-
-        const $pTags = $('p')
-        const $p_clean = $p.replace(/\s+/g, ' ').trim()
-
-        return $p_clean
-
-    } catch (error) {
-        console.error(`Error fetching page: ${error}.`)
-    }
-}
-
-module.exports = {extract_first, extract_all}
+export {article_extract}
