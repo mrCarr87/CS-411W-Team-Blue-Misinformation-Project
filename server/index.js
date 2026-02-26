2d51e6ba601f8d4ad887c0857469ced44393a581
christopher-artis <carti002@odu.edu>
2026-02-25 21:25:16 -0500

    package updates

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   next()
 })
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use("/auth", authRoutes); // Account Creation/Authentication
